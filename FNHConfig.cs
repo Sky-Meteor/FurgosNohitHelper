9e7da8d4d78578117858a233de34e3dd80cab8aa
Sky-Meteor <2746501855@qq.com>
2022-12-20 12:22:51 +0800

    Save Toggles √

---
+++ FNHConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
+
+namespace FurgosNohitHelper
+{
+    public class FNHConfig : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ClientSide;
+
+        [Header("指令相关选项")]
+        [Label("加载魂石选项提示每行显示条数")]
+        [DefaultValue(10)]
+        public int CommandTogglePerLine;
+    }
+}
--- FurgoCommand.cs
+++ FurgoCommand.cs
@@ -1,5 +1,4 @@
 ﻿using FargowiltasSouls.Toggler;
-using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -8,8 +7,8 @@ using Terraria.IO;
 using Terraria.ModLoader;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using Mono.Cecil.Mdb;
 using System.Linq;
+using Terraria.Localization;
 
 namespace FurgosNohitHelper
 {
@@ -63,11 +62,12 @@ namespace FurgosNohitHelper
 					}
 					foreach (List<string> list in CustomSettings)
 					{
-						Main.NewText($"{list[0]}：{list}"); // WIP
+                        Main.NewText(GetNamesFromList(list));
+                        
                     }
 					break;
 				case "names":
-					Main.NewText(NameCollection); // WIP
+                    Main.NewText(GetNamesFromList(NameCollection));
 					break;
 				case "load":
 					break;
@@ -123,16 +123,117 @@ namespace FurgosNohitHelper
 			CustomSettingsPath = null;
 		}
 
-		void GetNamesFromList(List<string> list)
+		static string GetNamesFromList(List<string> list)
 		{
-
-		} // WIP （先在这画个饼.jpg 
+            if (list.Count == 0)
+                return "";
+            string output = $"{list[0]}：";
+            int count = 0;
+            for(int i = 1; i < list.Count; i++)
+            {
+                if (count >= ModContent.GetInstance<FNHConfig>().CommandTogglePerLine)
+                {
+                    output += "\n";
+                    count = 0;
+                }
+                string text = LocalizationLoader.GetOrCreateTranslation(ModLoader.GetMod("FargowiltasSouls"), $"{list[i]}Config").GetTranslation(Language.ActiveCulture);
+                string key = text[text.IndexOf("{")..(text.IndexOf("}") + 1)];
+                text = text.Replace(key, Language.GetTextValue(key[2..(key.Length - 1)]));
+                output += $"{text}，";
+                count += 1;
+            }
+            return output[..(output.Length - 1)];
+		}
 
 		public List<List<string>> CustomSettings;
 		public List<string> NameCollection;
 
-		public static Preferences CustomSettingsPath;
+		public Preferences CustomSettingsPath;
 		internal static string path = Path.Combine(Main.SavePath, "ModConfigs", "FurgosNohitHelper_CustomSettings.json");
-		//internal static List<string> AllKeys;
-	}
+        #region EnchColors
+        public readonly static Dictionary<string, string> EnchColors = new()
+        {
+            { "BorealConfig", "8B7464" },
+            { "MahoganyConfig", "b56c64" },
+            { "EbonConfig", "645a8d" },
+            { "ShadeConfig", "586876" },
+            { "ShadeOnHitConfig", "586876" },
+            { "PalmConfig", "b78d56" },
+            { "PearlConfig", "ad9a5f" },
+
+            { "AdamantiteConfig", "dd557d" },
+            { "CobaltConfig", "3da4c4" },
+            { "AncientCobaltConfig", "354c74" },
+            { "MythrilConfig", "9dd290" },
+            { "OrichalcumConfig", "eb3291" },
+            { "PalladiumConfig", "f5ac28" },
+            { "PalladiumOrbConfig", "f5ac28" },
+            { "TitaniumConfig", "828c88" },
+
+            { "CopperConfig", "d56617" },
+            { "IronMConfig", "988e83" },
+            { "SilverSConfig", "b4b4cc" },
+            { "TinConfig", "a28b4e" },
+            { "TungstenConfig", "b0d2b2" },
+            { "TungstenProjConfig", "b0d2b2" },
+            { "ObsidianConfig", "453e73" },
+
+            { "GladiatorConfig", "9c924e" },
+            { "GoldConfig", "e7b21c" },
+            { "GoldToPiggyConfig", "e7b21c" },
+            { "HuntressConfig", "7ac04c" },
+            { "RedRidingRainConfig", "c01b3c" },
+            { "ValhallaConfig", "93651e" },
+            { "SquirePanicConfig", "948f8c" },
+
+            { "BeeConfig", "FEF625" },
+            { "BeetleConfig", "6D5C85" },
+            { "CactusConfig", "799e1d" },
+            { "PumpkinConfig", "e3651c" },
+            { "SpiderConfig", "6d4e45" },
+            { "TurtleConfig", "f89c5c" },
+
+            { "ChlorophyteConfig", "248900" },
+            { "CrimsonConfig", "C8364B" },
+            { "RainConfig", "ffec00" },
+            { "RainInnerTubeConfig", "ffec00" },
+            { "FrostConfig", "7abdb9" },
+            { "JungleConfig", "71971f" },
+            { "JungleDashConfig", "71971f" },
+            { "MoltenConfig", "c12b2b" },
+            { "MoltenEConfig", "c12b2b" },
+            { "ShroomiteConfig", "008cf4" },
+            { "ShroomiteShroomConfig", "008cf4" },
+
+            { "DarkArtConfig", "9b5cb0" },
+            { "ApprenticeConfig", "5d86a6" },
+            { "NecroConfig", "565643" },
+            { "NecroGloveConfig", "565643" },
+            { "ShadowConfig", "42356f" },
+            { "AncientShadowConfig", "42356f" },
+            { "MonkConfig", "920520" },
+            { "ShinobiDashConfig", "935b18" },
+            { "ShinobiConfig", "935b18" },
+            { "SpookyConfig", "644e74" },
+            { "NinjaSpeedConfig", "565643" },
+            { "CrystalDashConfig", "249dcf" },
+
+            { "FossilConfig", "8c5c3b" },
+            { "ForbiddenConfig", "e7b21c" },
+            { "HallowDodgeConfig", "968564" },
+            { "HallowedConfig", "968564" },
+            { "HallowSConfig", "968564" },
+            { "SpectreConfig", "accdfc" },
+            { "TikiConfig", "56A52B" },
+
+            { "MeteorConfig", "5f4752" },
+            { "NebulaConfig", "fe7ee5" },
+            { "SolarConfig", "fe9e23" },
+            { "SolarFlareConfig", "fe9e23" },
+            { "StardustConfig", "00aeee" },
+            { "VortexSConfig", "00f2aa" },
+            { "VortexVConfig", "00f2aa" }
+        };
+        #endregion
+    }
 }
